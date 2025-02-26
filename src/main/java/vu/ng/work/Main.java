635025bc8fe38c76653712d386b8f62379bc4af9
Maxim Shibanov <shibanovmax05@mail.ru>
2025-02-26 19:13:43 +0300

    исправил ошибку чтения

--- src/main/java/vu/ng/work/Main.java
+++ src/main/java/vu/ng/work/Main.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException{
-        Path fileName = Path.of("C:\\Users\\Максим\\Downloads\\Polygon_remover-14f186fa92967ec7c53fcce519f4506c02eb6221\\Polygon_remover-14f186fa92967ec7c53fcce519f4506c02eb6221\\src\\caracal_cube.obj");
+        Path fileName = Path.of("C:\\Users\\Максим\\Downloads\\task3\\task3\\src\\caracal_cube.obj");
         String fileContent = Files.readString(fileName);
 
         System.out.println("Загрузка модели ...");
@@ -21,7 +21,7 @@ public class Main {
         System.out.println("Вершины: " + model.vertices.size());
         System.out.println("Полигоны: " + model.polygons.size());
 
-        PolygonRemover.removePolygons(model, List.of(0,1,2,3/4));
+        PolygonRemover.removePolygons(model, List.of(2,3,4));
         ObjWriterClass objwriter = new ObjWriterClass();
         objwriter.write(model,"C:\\Users\\Максим\\Downloads\\Polygon_remover-14f186fa92967ec7c53fcce519f4506c02eb6221\\Polygon_remover-14f186fa92967ec7c53fcce519f4506c02eb6221\\caracal_2.obj");
         System.out.println("Вершины: " + model.vertices.size());
